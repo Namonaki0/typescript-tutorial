9944ee479f77b1d1d4457857fe35f2649d06c178
Andre <andreferreira@live.co.uk>
2024-08-17 16:37:19 +0100

    typeof

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -415,7 +415,7 @@ console.log(getColorName(Color.Green));
 
 
 import newStudentName, { sayHello, personName, type Student } from "./actions";
-import { example } from "./example";    
+import { example } from "./example";
 
 sayHello('testName');
 console.log(newStudentName);
@@ -427,3 +427,28 @@ const anotherStudent: Student = {
 }
 
 console.log(anotherStudent);
+
+
+
+// typeof
+type ValueType = string | number | boolean;
+
+let value: ValueType;
+const random = Math.random();
+value = random < 0.33 ? 'Hello' : random < 0.66 ? 123.456 : true;
+
+function checkValue(value: ValueType):void {
+ if (typeof value === 'string') {
+    console.log(value.toLowerCase());
+    return;
+ }
+
+ if (typeof value === 'number') {
+    console.log(value.toFixed(2));
+    return;
+ }
+
+ console.log(`Boolean : ${value}`);
+}
+
+checkValue(value)
